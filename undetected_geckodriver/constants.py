f3e6585c36240ee4d9bef647fd2fae06b1aa7c93
reihan <reiyhan.aditya@gmail.com>
2025-04-12 14:20:15 +0700

    windows path

--- undetected_geckodriver/constants.py
+++ undetected_geckodriver/constants.py
@@ -3,12 +3,12 @@ TO_REPLACE_STRING = b"webdriver"
 
 PLATFORM_DEPENDENT_PARAMS = {
     # Do not remove, the (actual) support for Windows & macOS is coming soon
-    # "Windows": {
-    #    "firefox_execs": ["firefox.exe"],
-    #    "firefox_paths": ["C:\\Program Files\\Mozilla Firefox"],
-    #    "undetected_path": "C:\\Users\\{USER}\\AppData\\Local\\undetected_firefox\\",
-    #    "xul": "xul.dll",
-    # },
+    "Windows": {
+        "firefox_execs": ["firefox.exe"],
+        "firefox_paths": ["C:\\Program Files\\Mozilla Firefox"],
+        "undetected_path": "{HOME_PATH}\\AppData\\Local\\undetected_firefox\\",
+        "xul": "xul.dll",
+    },
     # "Darwin": {
     #    "firefox_execs": ["Firefox.app"],
     #    "firefox_paths": ["/Applications/Firefox.app/Contents/MacOS"],
